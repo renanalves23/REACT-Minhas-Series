2ff9d5df6b359c8e29e341a1d4331ab5329381e7
renan <renanalves23@outlook.com>
2020-03-03 19:45:12 -0300

    State e onChange para NovoGenero OK

--- src/NovoGenero.js
+++ src/NovoGenero.js
@@ -2,15 +2,22 @@ import React, { useState } from 'react'
 
 
 const NovoGenero = () => {
+  const [name, setName] = useState('')
+
+  const onChange = event =>{
+    setName(event.target.value)
+  }
+
   return(
     <div className="container">
       <h1>Novo Gênero</h1>
       <form>
-          <div class="form-group">
-            <label for="name">Nome</label>
-            <input type="text" className="form-control" id="name" placeholder='Nome do Gênero' />
+      {name}
+          <div className="form-group">
+            <label htmlFor="name">Nome</label>
+            <input type="text" value={name} onChange={onChange} className="form-control" id="name" placeholder='Nome do Gênero' />
       </div>
-      <button type="button" className="btn btn-primary">Salvar</button>
+      <button type="button" className="btn btn-lg btn-primary">Salvar</button>
       </form>
     </div>
   )
