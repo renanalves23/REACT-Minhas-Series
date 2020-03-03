034cf9a2d40b676354adf20b15402d5abd24d0b0
renan <renanalves23@outlook.com>
2020-03-03 17:10:07 -0300

    commit antes de passar os dados da api para a tabela

--- src/Generos.js
+++ src/Generos.js
@@ -9,10 +9,26 @@ const Generos = () => {
       setData(res.data.data)
     })
   }, [])
-
+//
   return (
     <div>
        <h1 path='/api/genres'>Generos</h1>
+       <table className="table table-dark">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Açoes</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td></td>
+              </tr>
+            </tbody>
+        </table>
        <pre>{JSON.stringify(data)}</pre>
     </div>
     )
